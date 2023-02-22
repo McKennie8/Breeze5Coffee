23dd234d2d65bd9204fa345aa9b9963d29d46b66
Kirby <110064066+McKennie8@users.noreply.github.com>
2023-02-21 20:17:18 -0500

    3. failed slider dynamic blocks

--- assets/global.js
+++ assets/global.js
@@ -1323,6 +1323,45 @@ class ProductRecommendations extends HTMLElement {
 
 customElements.define("product-recommendations", ProductRecommendations);
 
+// $(document).ready(function () {
+//   $(".testimonial-slider").slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     responsive: [
+//       {
+//         breakpoint: 1024,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 3,
+//           infinite: true,
+//           dots: true,
+//           arrows: false,
+//         },
+//       },
+//       {
+//         breakpoint: 989,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//         },
+//       },
+//       {
+//         breakpoint: 750,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       // You can unslick at a given breakpoint now by adding:
+//       // settings: "unslick"
+//       // instead of a settings object
+//     ],
+//   });
+// });
+
 $(document).ready(function () {
   $(".testimonial-slider").slick({
     dots: true,
@@ -1355,9 +1394,6 @@ $(document).ready(function () {
           slidesToScroll: 1,
         },
       },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ],
   });
 });
